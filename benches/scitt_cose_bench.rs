4a0c0444ba49122f914b66edf06ea17d2d9ed24e
João Gonçalves <joaogoncalves8@hotmail.com>
2025-07-29 16:18:34 +0100

    add profiling to cose and policy validations

--- benches/scitt_cose_bench.rs
+++ benches/scitt_cose_bench.rs
@@ -1,13 +1,28 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
+#[cfg(feature = "policy_validation")]
 static COSE_CLAIM: &[u8] = include_bytes!("assets/claim.cose");
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("validate_scitt_cose_signed_statement", |b| {
-        b.iter(|| {
+
+    #[cfg(feature = "policy_validation")] {
+        c.bench_function("cose_validation", |b| {
+            b.iter(|| {
+                let _ = scitt_cose::validate_scitt_cose_signed_statement(COSE_CLAIM);
+            })
+        });
+
+        let guard = pprof::ProfilerGuard::new(100).unwrap();
+        for _ in 0..10000 {
             let _ = scitt_cose::validate_scitt_cose_signed_statement(COSE_CLAIM);
-        })
-    });
+        }
+        if let Ok(report) = guard.report().build() {
+            let file = std::fs::File::create("benches/profiling/cose_validation.svg").unwrap();
+            report.flamegraph(file).unwrap();
+        } else {
+            eprintln!("Failed to generate flamegraph report");
+        }
+    }
 }
 
 criterion_group!(benches, criterion_benchmark);
--- benches/scitt_policy_validation.rs
+++ benches/scitt_policy_validation.rs
@@ -1,19 +1,37 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
+#[cfg(feature = "policy_validation")]
 static POLICY: &str = include_str!("assets/policy.js");
+#[cfg(feature = "policy_validation")]
 static COSE_CLAIM: &[u8] = include_bytes!("assets/claim.cose");
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("policy_validation", |b| {
+    #[cfg(feature = "policy_validation")] {
+        c.bench_function("policy_validation", |b| {
+            let runtime = rquickjs::Runtime::new().unwrap();
+            let policy = POLICY.as_bytes();
+            let headers = scitt_cose::validate_scitt_cose_signed_statement(COSE_CLAIM)
+                .expect("Failed to parse COSE claim");
+            let phdr = headers.phdr;
+            b.iter(|| {
+                let _ = pft::consensus::engines::scitt::apply_policy_to_claim(&runtime, policy, &phdr);
+            })
+        });
+
         let runtime = rquickjs::Runtime::new().unwrap();
         let policy = POLICY.as_bytes();
         let headers = scitt_cose::validate_scitt_cose_signed_statement(COSE_CLAIM)
             .expect("Failed to parse COSE claim");
         let phdr = headers.phdr;
-        b.iter(|| {
-            pft::consensus::engines::scitt::apply_policy_to_claim(&runtime, policy, &phdr);
-        })
-    });
+        let guard = pprof::ProfilerGuard::new(100).unwrap();
+        for _ in 0..10000 {
+            let _ = pft::consensus::engines::scitt::apply_policy_to_claim(&runtime, policy, &phdr);
+        }
+        if let Ok(report) = guard.report().build() {
+            let file = std::fs::File::create("benches/profiling/policy_validation.svg").unwrap();
+            report.flamegraph(file).unwrap();
+        }
+    }
 }
 
 criterion_group!(benches, criterion_benchmark);
