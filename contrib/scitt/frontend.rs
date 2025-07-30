2c4ee30d9794b9cd8fc77ffbbe35182818abf1a6
João Gonçalves <joaogoncalves8@hotmail.com>
2025-07-30 10:37:45 +0100

    refactor scitt send/recv

--- contrib/scitt/frontend.rs
+++ contrib/scitt/frontend.rs
@@ -2,23 +2,27 @@ use crate::cbor_utils::operation_props_to_cbor;
 
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-use rustls::{ServerConfig, Certificate, PrivateKey};
-use rustls_pemfile::{certs, pkcs8_private_keys, rsa_private_keys};
 use log::warn;
 use pft::config::Config;
 use pft::consensus::app::TxWithValidationAck;
 use pft::consensus::batch_proposal::TxWithAckChanTag;
 use pft::consensus::engines::scitt::{SCITTWriteType, TXID};
-use pft::proto::client::{self, ProtoClientReply, ProtoTransactionResponse};
+use pft::proto::client::{
+    self, ProtoClientReply, ProtoTransactionReceipt, ProtoTransactionResponse,
+};
 use pft::proto::execution::{ProtoTransaction, ProtoTransactionOp, ProtoTransactionPhase};
 use pft::rpc::SenderType;
 use pft::utils::channel::Sender;
 use prost::Message;
-use serde::Deserialize;
-use std::{collections::HashMap, fs::File, io::BufReader};
+use rustls::{Certificate, PrivateKey, ServerConfig};
+use rustls_pemfile::{certs, pkcs8_private_keys, rsa_private_keys};
+use serde::{de, Deserialize};
 use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::Arc;
+use std::{collections::HashMap, fs::File, io::BufReader};
 use tokio::sync::{mpsc, oneshot, Mutex};
+
+static AUDIT_RECEIPT: bool = cfg!(not(feature = "commit_receipts"));
 
 struct AppState {
     /// Global channel to feed into the consensusNode.
@@ -65,7 +69,7 @@ async fn register_signed_statement(
         }
     }
 
-    let response = match send(vec![transaction_op], &state, true).await {
+    let response = match send_write(vec![transaction_op], &state).await {
         Ok(response) => response,
         Err(err) => return err,
     };
@@ -95,30 +99,16 @@ async fn get_entry_receipt(txid: web::Path<String>, state: web::Data<AppState>) 
         None => return HttpResponse::BadRequest().body("Invalid txid"),
     };
 
-    let transaction_op = ProtoTransactionOp {
-        op_type: {
-            if cfg!(feature = "commit_receipts") {
-                pft::proto::execution::ProtoTransactionOpType::ProbeCommit.into()
-            } else {
-                pft::proto::execution::ProtoTransactionOpType::ProbeAudit.into()
-            }
-        },
-        operands: vec![txid.to_vec()],
-    };
-
-    let response = match send_read(vec![transaction_op], &state).await {
-        Ok(response) => response,
+    let _receipt = match get_receipt(&txid, &state, AUDIT_RECEIPT).await {
+        Ok(receipt) => receipt,
         Err(err) => return err,
     };
 
-    let results = response.results.unwrap().result;
-    if results[0].values.is_empty() {
-        return HttpResponse::NotFound().body("No results found for the given txid");
-    }
+    // TODO receipt to cose
 
     HttpResponse::Ok()
         .content_type("application/cose")
-        .body(results[0].values[0].clone())
+        .body(vec![])
 }
 
 /// Retrieve Statement with Embedded Receipt
@@ -147,6 +137,13 @@ async fn get_entry_statement(
     if results[0].values.is_empty() {
         return HttpResponse::NotFound().body("No results found for the given txid");
     }
+
+    let _receipt = match get_receipt(&txid, &state, AUDIT_RECEIPT).await {
+        Ok(receipt) => receipt,
+        Err(err) => return err,
+    };
+
+    // TODO embed the receipt in the response
 
     HttpResponse::Ok()
         .content_type("application/cose")
@@ -242,16 +239,10 @@ async fn get_operation_with_status(
 /// Policy Registration
 /// The endpoint itself if not part of the spec, this is a simplified solution in comparison with the CCF governance model
 #[post("/policy")]
-async fn register_policy(
-    policy: web::Bytes,
-    state: web::Data<AppState>,
-) -> impl Responder {
+async fn register_policy(policy: web::Bytes, state: web::Data<AppState>) -> impl Responder {
     let transaction_op = ProtoTransactionOp {
         op_type: pft::proto::execution::ProtoTransactionOpType::Write.into(),
-        operands: vec![
-            SCITTWriteType::Policy.to_slice().to_vec(),
-            policy.to_vec(),
-        ],
+        operands: vec![SCITTWriteType::Policy.to_slice().to_vec(), policy.to_vec()],
     };
 
     let (tx, rx) = oneshot::channel();
@@ -267,14 +258,53 @@ async fn register_policy(
         }
     }
 
-    let _result = match send(vec![transaction_op], &state, true).await {
+    let result = match send_write(vec![transaction_op], &state).await {
         Ok(response) => response,
         Err(err) => return err,
     };
 
+    let txid = TXID {
+        block_n: result.block_n,
+        tx_idx: TryFrom::try_from(result.tx_n).unwrap(),
+    };
+
+    if let Err(err) = get_receipt(&txid, &state, true).await {
+        warn!("Failed to get receipt for policy registration: {:?}", err);
+        return err;
+    }
+
     HttpResponse::Ok()
         .content_type("application/text")
         .body("Policy registered successfully.")
+}
+
+async fn get_receipt(
+    txid: &TXID,
+    state: &AppState,
+    audit: bool,
+) -> Result<ProtoTransactionReceipt, HttpResponse> {
+    let transaction_op = ProtoTransactionOp {
+        op_type: if audit {
+            pft::proto::execution::ProtoTransactionOpType::ProbeAudit.into()
+        } else {
+            pft::proto::execution::ProtoTransactionOpType::ProbeCommit.into()
+        },
+        operands: vec![txid.block_n.to_be_bytes().to_vec(), txid.tx_idx.to_be_bytes().to_vec()],
+    };
+
+    let transaction_phase = ProtoTransactionPhase {
+        ops: vec![transaction_op],
+    };
+
+    let transaction = ProtoTransaction {
+        on_receive: Some(transaction_phase),
+        on_crash_commit: None,
+        on_byzantine_commit: None,
+        is_reconfiguration: false,
+        is_2pc: false,
+    };
+
+    unwrap_receipt(base_send(transaction, state).await?)
 }
 
 async fn send_read(
@@ -293,13 +323,12 @@ async fn send_read(
         is_2pc: false,
     };
 
-    base_send(transaction, state).await
+    unwrap_response(base_send(transaction, state).await?)
 }
 
-async fn send(
+async fn send_write(
     transaction_ops: Vec<ProtoTransactionOp>,
     state: &AppState,
-    byz_commit_probe: bool,
 ) -> Result<ProtoTransactionResponse, HttpResponse> {
     let transaction_phase = ProtoTransactionPhase {
         ops: transaction_ops,
@@ -313,53 +342,13 @@ async fn send(
         is_2pc: false,
     };
 
-    let response = base_send(transaction, state).await?;
-
-    if response.block_n != 0 && byz_commit_probe {
-        let current_tag = state.curr_client_tag.fetch_add(1, Ordering::AcqRel);
-
-        let probe_transaction = ProtoTransaction {
-            on_receive: Some(ProtoTransactionPhase {
-                ops: vec![ProtoTransactionOp {
-                    op_type: {
-                        if cfg!(feature = "commit_receipts") {
-                            pft::proto::execution::ProtoTransactionOpType::ProbeCommit.into()
-                        } else {
-                            pft::proto::execution::ProtoTransactionOpType::ProbeAudit.into()
-                        }
-                    },
-                    operands: vec![
-                        response.block_n.to_be_bytes().to_vec(),
-                        response.tx_n.to_be_bytes().to_vec(),
-                    ],
-                }],
-            }),
-            on_crash_commit: None,
-            on_byzantine_commit: None,
-            is_reconfiguration: false,
-            is_2pc: false,
-        };
-
-        let (tx, mut rx) = mpsc::channel(1);
-        let tx_with_ack_chan_tag: TxWithAckChanTag =
-            (Some(probe_transaction), (tx, current_tag, SenderType::Anon));
-        state
-            .batch_proposer_tx
-            .send(tx_with_ack_chan_tag)
-            .await
-            .unwrap();
-
-        let _ = rx.recv().await;
-
-        // Probe replies only after Byz commit
-    }
-    Ok(response)
+    unwrap_response(base_send(transaction, state).await?)
 }
 
 async fn base_send(
     transaction: ProtoTransaction,
     state: &AppState,
-) -> Result<ProtoTransactionResponse, HttpResponse> {
+) -> Result<ProtoClientReply, HttpResponse> {
     let current_tag = state.curr_client_tag.fetch_add(1, Ordering::AcqRel);
 
     let (tx, mut rx) = mpsc::channel(1);
@@ -380,13 +369,18 @@ async fn base_send(
 
     let resp = resp.as_ref();
 
-    let decoded_payload = match ProtoClientReply::decode(&resp.0.as_slice()[0..resp.1]) {
-        Ok(payload) => payload,
+    match ProtoClientReply::decode(&resp.0.as_slice()[0..resp.1]) {
+        Ok(payload) => Ok(payload),
         Err(e) => {
             warn!("Error decoding response: {}", e);
-            return Err(HttpResponse::InternalServerError().body("Error decoding response"));
+            Err(HttpResponse::InternalServerError().body("Error decoding response"))
         }
-    };
+    }
+}
+
+fn unwrap_response(
+    decoded_payload: ProtoClientReply,
+) -> Result<ProtoTransactionResponse, HttpResponse> {
     match decoded_payload.reply.unwrap() {
         client::proto_client_reply::Reply::Response(response) => Ok(response),
         client::proto_client_reply::Reply::TryAgain(ta) => Err(HttpResponse::ServiceUnavailable()
@@ -404,9 +398,47 @@ async fn base_send(
                 "Transaction accepted but not yet committed: {} {}",
                 tr.block_n, tr.tx_n
             ))),
-        client::proto_client_reply::Reply::CommitReceipt(_) =>
-            Err(HttpResponse::InternalServerError().body("Commit receipt not expected in this context")),
-        client::proto_client_reply::Reply::AuditReceipt(_) => Err(HttpResponse::InternalServerError().body("Audit receipt not expected in this context")),
+        client::proto_client_reply::Reply::CommitReceipt(_) => {
+            Err(HttpResponse::InternalServerError()
+                .body("Commit receipt not expected in this context"))
+        }
+        client::proto_client_reply::Reply::AuditReceipt(_) => {
+            Err(HttpResponse::InternalServerError()
+                .body("Audit receipt not expected in this context"))
+        }
+    }
+}
+
+fn unwrap_receipt(
+    decoded_payload: ProtoClientReply,
+) -> Result<ProtoTransactionReceipt, HttpResponse> {
+    match decoded_payload.reply.unwrap() {
+        client::proto_client_reply::Reply::AuditReceipt(receipt) => Ok(receipt),
+        client::proto_client_reply::Reply::CommitReceipt(receipt) => Ok(receipt),
+        client::proto_client_reply::Reply::Response(proto_transaction_response) => {
+            Err(HttpResponse::InternalServerError().body(format!(
+                "Unexpected response type: {:?}",
+                proto_transaction_response
+            )))
+        }
+        client::proto_client_reply::Reply::TryAgain(proto_try_again) => {
+            Err(HttpResponse::ServiceUnavailable().body(format!(
+                "Service temporarily unavailable, please try again: {}",
+                proto_try_again.serialized_node_infos
+            )))
+        }
+        client::proto_client_reply::Reply::Leader(proto_current_leader) => {
+            Err(HttpResponse::TemporaryRedirect().body(format!(
+                "Request should be sent to the leader node {}",
+                proto_current_leader.name
+            )))
+        }
+        client::proto_client_reply::Reply::TentativeResponse(proto_tentative_response) => {
+            Err(HttpResponse::Accepted().body(format!(
+                "Transaction accepted but not yet committed: {} {}",
+                proto_tentative_response.block_n, proto_tentative_response.tx_n
+            )))
+        }
     }
 }
 
@@ -425,22 +457,28 @@ pub async fn run_actix_server(
 
     let batch_size = config.consensus_config.max_backlog_batch_size.max(256);
 
-    let cert_file = &mut BufReader::new(File::open(&config.net_config.tls_cert_path).expect("Invalid TLS cert file path"));
-    let key_file = &mut BufReader::new(File::open(&config.net_config.tls_key_path).expect("Invalid TLS key file path"));
+    let cert_file = &mut BufReader::new(
+        File::open(&config.net_config.tls_cert_path).expect("Invalid TLS cert file path"),
+    );
+    let key_file = &mut BufReader::new(
+        File::open(&config.net_config.tls_key_path).expect("Invalid TLS key file path"),
+    );
 
     let cert_chain: Vec<Certificate> = certs(cert_file)
         .expect("Invalid TLS cert file format")
         .into_iter()
         .map(Certificate)
         .collect();
-    
+
     let mut keys: Vec<PrivateKey> = rsa_private_keys(key_file)
         .unwrap_or_else(|_| Vec::new())
         .into_iter()
         .map(PrivateKey)
         .collect();
     if keys.is_empty() {
-        let key_file = &mut BufReader::new(File::open(&config.net_config.tls_key_path).expect("Invalid TLS key file path"));
+        let key_file = &mut BufReader::new(
+            File::open(&config.net_config.tls_key_path).expect("Invalid TLS key file path"),
+        );
         keys = pkcs8_private_keys(key_file)
             .expect("Invalid TLS key file format - not RSA or PKCS8")
             .into_iter()
